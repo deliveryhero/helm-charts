7bc6d718c6750e53ecb7d748013760501ee830f5
linzhengen <linzhengen@yahoo.co.jp>
2021-04-21 22:01:36 +0900

    helm-docs: bump version to v1.5.0 & update README.md

--- ci/helm-docs.sh
+++ ci/helm-docs.sh
@@ -8,7 +8,7 @@ rm -f .helm-docs-md5sum-before .helm-docs-md5sum-after
 md5sum stable/*/README.md > .helm-docs-md5sum-before
 
 # Run helm-docs to generate all README.md files from the template
-helm-docs --template-file=../../ci/README.md.gotmpl
+helm-docs --template-files ./ci/README.md.gotmpl
 
 # Get md5sum of all README.md files after running helm-docs
 md5sum stable/*/README.md > .helm-docs-md5sum-after
