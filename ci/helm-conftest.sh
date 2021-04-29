43af0c2403ad8a2e9d32c868c8400f540c28f878
Max Williams <max.williams@deliveryhero.com>
2021-04-29 17:24:26 +0200

    Fix helm conftest CI test

--- ci/helm-conftest.sh
+++ ci/helm-conftest.sh
@@ -9,5 +9,5 @@ for chart in $(find stable -maxdepth 1 -mindepth 1); do
   echo -n "helm-conftest running for chart: ${chart}..."
   # Remove any dependencies as we are not going to test them
   rm -f "${chart}/requirements.yaml"
-  helm template "${chart}" | conftest -p ci/helm-conftest-policies test - && echo "OK"
+  helm template "${chart}" | conftest -p ci/helm-conftest-policies test -
 done
