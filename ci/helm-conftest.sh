b8b218fd489cb14b5034a322f8c0a499b4d67aa6
Max Williams <max.williams@deliveryhero.com>
2021-07-09 19:47:20 +0200

    Hack ci script to install helm3 if required

--- ci/helm-conftest.sh
+++ ci/helm-conftest.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
+# Check if we are running in Github actions and apply some required changes
 if [[ ! -z "${GITHUB_RUN_ID}" ]]; then
   alias conftest=/root/.helm/plugins/helm-conftest/bin/conftest
   helm repo remove local
   set -euo pipefail
+fi
+
+# Check if we are running in docker and ensure we have Helm3 if so
+if [ -f "/.dockerenv" ]; then
+  if helm version --client --short | grep -q 'v2.14'; then
+    echo "Running in docker but Helm version 2 found, will install Helm 3..."
+    export VERIFY_CHECKSUM=false
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh --version "v3.6.2"
+  fi
 fi
 
 for chart in $(find stable -maxdepth 1 -mindepth 1); do
--- stable/backstage/README.md
+++ stable/backstage/README.md
@@ -1,6 +1,6 @@
 # backstage
 
-![Version: 0.1.11](https://img.shields.io/badge/Version-0.1.11-informational?style=flat-square) ![Type: application](https://img.shields.io/badge/Type-application-informational?style=flat-square) ![AppVersion: v0.1.1-alpha.23](https://img.shields.io/badge/AppVersion-v0.1.1--alpha.23-informational?style=flat-square)
+![Version: 0.1.12](https://img.shields.io/badge/Version-0.1.12-informational?style=flat-square) ![Type: application](https://img.shields.io/badge/Type-application-informational?style=flat-square) ![AppVersion: v0.1.1-alpha.23](https://img.shields.io/badge/AppVersion-v0.1.1--alpha.23-informational?style=flat-square)
 
 A Helm chart for Backstage
 
