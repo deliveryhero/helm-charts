0b420820e3f88d5e7f9f10ddd1c9a029adb9a422
Brenno Oliveira <brenno.oliveira@deliveryhero.com>
2024-08-19 16:55:14 +0200

    [stable/none] fix: helm-conftest step (#598)
    * fix: helm-conftest step
    
    Signed-off-by: Brenno Oliveira <brenno.oliveira@deliveryhero.com>
    
    * fix conftest download url
    
    Signed-off-by: Brenno Oliveira <brenno.oliveira@deliveryhero.com>
    
    ---------
    
    Signed-off-by: Brenno Oliveira <brenno.oliveira@deliveryhero.com>

--- ci/helm-conftest.sh
+++ ci/helm-conftest.sh
@@ -2,22 +2,6 @@
 
 set -euo pipefail
 
-# Check if we are running in docker and ensure we have Helm3 if so
-if [ -f "/.dockerenv" ]; then
-  CONFTEST="/root/.helm/plugins/helm-conftest/bin/conftest"
-  if helm version --client --short | grep -q 'v2.14'; then
-    echo "Running in docker but Helm version 2 found, will install Helm 3..."
-    export VERIFY_CHECKSUM=false
-    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-    chmod 700 get_helm.sh
-    ./get_helm.sh --version "v3.6.2"
-    echo "Helm version now installed: "
-    helm version
-  fi
-else
-  CONFTEST=$(which conftest)
-fi
-
 while IFS= read -r -d '' chart
 do
   echo "=============================================================="
@@ -25,5 +9,5 @@ do
   # Remove any dependencies as we are not going to test them
   rm -f "${chart}/requirements.yaml"
   rm -rf "${chart}/charts"
-  helm template "${chart}" | $CONFTEST -p ci/helm-conftest-policies test -
+  helm template "${chart}" | conftest -p ci/helm-conftest-policies test -
 done < <(find stable -maxdepth 1 -mindepth 1 -print0)
