758f430469837b3858b7b019b758f1ee917e4466
Max Williams <max-rocket-internet@users.noreply.github.com>
2024-10-22 09:31:05 +0200

    uploading old chart versions to GHCR

---
+++ ci/upload-old-versions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+CHARTS=$(ls -1 stable/)
+
+count=0
+
+while true; do
+  echo "."
+  for chart in $CHARTS; do
+    chart_env_name=$(echo $chart | sed 's/-/_/g')
+    current_version=$(yq '.version' stable/$chart/Chart.yaml)
+    previous_version_env_name=$chart_env_name
+    previous_version=${!previous_version_env_name}
+    # echo "chart_env_name $chart_env_name current_version $current_version previous_version $previous_version"
+    if [[ "$current_version" == "$previous_version" ]]; then
+    # echo "skipping chart $chart, version $current_version is the same as previous ($previous_version)"
+      continue
+    fi
+    echo "Building and pushing $chart $current_version"
+    # helm dep up "stable/$chart"
+    helm package "stable/$chart" -u
+    package_file=$(ls "$chart"*.tgz)
+    echo "Running: helm push $package_file oci://ghcr.io/deliveryhero/helm-charts"
+    export $(echo "$(echo ${chart_env_name})=$current_version")
+    rm -f $package_file
+  done
+  echo "Moving to previous commit.."
+  previous_commit=$(git log --oneline | head -2 | tail -1 | cut -f1 -d' ')
+  git reset --hard $previous_commit --quiet
+  # sleep 5
+  count=$((count + 1))
+  echo "=================================================================================="
+  if [ "$count" -gt 3 ]; then
+    echo "done"
+    exit 0
+  fi
+done
