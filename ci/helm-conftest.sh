3ec159658ac96309b49b513a97f3f33aad6aee6f
Max Williams <8859277+max-rocket-internet@users.noreply.github.com>
2025-01-07 16:49:25 +0100

    [stable/listmonk] add artifacthub annotation (#658)
    * [stable/listmonk] add artifacthub annotation
    
    * remove dependencies from Chart.yaml when present in order to run conftest
    
    ---------
    
    Co-authored-by: Max Williams <max-rocket-internet@users.noreply.github.com>

--- ci/helm-conftest.sh
+++ ci/helm-conftest.sh
@@ -6,8 +6,11 @@ while IFS= read -r -d '' chart
 do
   echo "=============================================================="
   echo "helm-conftest running for chart: ${chart}..."
+
   # Remove any dependencies as we are not going to test them
   rm -f "${chart}/requirements.yaml"
   rm -rf "${chart}/charts"
+  yq -i 'del(.dependencies)' "${chart}/Chart.yaml"
+
   helm template "${chart}" | conftest -p ci/helm-conftest-policies test -
 done < <(find stable -maxdepth 1 -mindepth 1 -print0)
